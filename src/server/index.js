5fd572dc96aabefdbaca1997fa1ab1715ea73f8b
Megoos <m.guskov@dooglys.com>
2018-04-20 11:58:31 +0300

    docker pull/tag with node

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,50 @@
 var app = require('http').createServer();
 var io = (module.exports.io = require('socket.io')(app));
 
-const PORT = process.env.PORT || 3231;
+var Docker = require('dockerode');
+var docker = new Docker({ socketPath: '/var/run/docker.sock' });
 
-const SocketManager = require('./SocketManager');
+const repo = 'ubuntu:16.04';
 
-io.on('connection', SocketManager);
+// const PORT = process.env.PORT || 3231;
 
-app.listen(PORT, () => {
-  console.log('Connected to port:' + PORT);
-});
+// const SocketManager = require('./SocketManager');
+
+// io.on('connection', SocketManager);
+
+// app.listen(PORT, () => {
+//   console.log('Connected to port:' + PORT);
+// });
+
+
+function checkImage(repo) {
+    const image = docker.getImage(repo);
+    image.inspect(function(err, data) {
+        if (err) return console.error('failed to inspect image: ' + err);
+
+        console.log('image ID: ' + data.Id);
+
+        image.tag(
+            {
+                repo: repo,
+                tag: 'latest'
+            },
+            (err, data) => {
+                if (err) return console.error('failed to tag image: ' + err);
+            }
+        );
+    });
+}
+
+// docker.pull(repo, function(err, stream) {
+//     docker.modem.followProgress(stream, onFinished, onProgress);
+//     function onFinished(err, output) {
+//         if (err) return console.error('failed to pull image:', err);
+
+//         console.log('done!');
+//         checkImage(repo);
+//     }
+//     function onProgress(event) {
+//         console.log('...');
+//     }
+// });
